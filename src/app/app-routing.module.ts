714b442a4dd18039430a1521355c6ed51619092b
Nagarjuna-Singamsetti <naga0955@gmail.com>
2022-07-20 22:44:19 +0530

    Second Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerAppComponent } from './Components/customer-app/customer-app.component';
+import { HomeViewComponent } from './Components/home-view/home-view.component';
+import { SupplierPageComponent } from './Components/supplier-page/supplier-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeViewComponent },
+  { path: 'Customers', component: CustomerAppComponent },
+  { path: 'Suppliers', component: SupplierPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerAppComponent } from './Components/customer-app/customer-app.component';
+import { HomeViewComponent } from './Components/home-view/home-view.component';
+import { MasterPageComponent } from './Components/master-page/master-page.component';
+import { SupplierPageComponent } from './Components/supplier-page/supplier-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerAppComponent
+    CustomerAppComponent,
+    HomeViewComponent,
+    MasterPageComponent,
+    SupplierPageComponent
   ],
   imports: [
     BrowserModule,
